7f2247baed6e2e9dd8ed4640125bd63a7392dbf9
fihar abdelali <fiharali0@gmail.com>
2024-03-09 13:10:23 +0100

    add ckeckUserStatus

--- resources/views/Auth/resetPassword.blade.php
+++ resources/views/Auth/resetPassword.blade.php
@@ -23,7 +23,7 @@
             <form class="mt-8 space-y-6" method="post" action="{{route('reset')}}">
                 @csrf
 
-                <input type="text" name="token" value="{{ $token }}">
+                <input type="hidden" name="token" value="{{ $token }}">
                 <div>
                     <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
                     <input type="email" name="email" id="email" value="{{$email}}" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="example@example.com">
